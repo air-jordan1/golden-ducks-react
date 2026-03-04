6d2f2d0e5bd9da0a31067224e0e6e3e17761423b
eunicehwang06 <eunicehwang06@gmail.com>
2026-03-04 03:04:35 +0000

    still working on settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,28 +2,45 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 function Settings() {
-  const bibleApis = ['ASV', 'Geneva', 'KJV'];
-  const [selectedApi, setSelectedApi] = useState(bibleApis[0]);
 
-  function FetchTranslation() {
-    fetch('https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-kjv/books/john/chapters/3/verses/16.json)', []);
-  }
+  var passage;
+  const bibleApis = ['ASV', 'Geneva', 'KJV'];
 
   function Translation() {
-    useEffect(FetchTranslation, []);
   }
 
-  function ChangeTranslation({translation}) {
-    return 
+  function TranslationController() {
+    function GetTranslation() {
+      useEffect( () =>
+        fetch('https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-kjv/books/john/chapters/3/verses/16.json)', [])
+        .then((response) => response.json())
+        .then((data) => passage = data.text )
+        , [])
+    }
+
+    function ChangeTranslation() {
+
+    }
+
+    return (
+      <div>
+        <p>Change translation?.</p>
+        <select>
+          {bibleApis.map(api => <option key={api}>{api}</option>)}
+        </select>
+      </div>
+    )
+  }
+
+  function DemoPassage() {
+    return verse1
   }
 
   return (
     <div>
       <h1>Settings</h1>
-      <p>Change translation?.</p>
-      <select>
-        {bibleApis.map(api => <option key={api}>{api}</option>)}
-      </select>
+      <TranslationController />
+      <p>{passage}</p>
     </div>
   );
 }
