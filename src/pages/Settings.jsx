934497872775e53e8fe9d9da0d4eb30a00822b48
eunicehwang06 <eunicehwang06@gmail.com>
2026-03-04 04:49:20 +0000

    updated style

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,48 +1,54 @@
-import React from 'react';
 import { useEffect, useState } from 'react';
 
 function Settings() {
 
-  function Translation() {
-
-  }
-
   function TranslationController() {
-    const bibleApis = ['ASV', 'Geneva', 'KJV'];
-    const [selectedApi, setSelectedApi] = useState(bibleApis[0]);
+    const selectorOptions = ['asv', 'gnv', 'kjv']; // selector options
+    const [version, setVersion] = useState(selectorOptions[0]); // version
+    const [selectedApi, setSelectedApi] = useState(null); // actual text
 
-    function GetTranslation() {
+    const handleSelection = (event) => {
+      setVersion(event.target.value);
+      changeTranslation(version);
+    };
 
+    function changeTranslation(version) {
+      useEffect(() => { 
+        const api = fetch("https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-" + version + "/books/john/chapters/3/verses/16.json", []);
+        const passage = api.json().text;
+        console.log(passage);
+        setSelectedApi(passage);
+      }, [version]);
     }
 
-    function ChangeTranslation(selectedApi) {
-      useEffect(() => { 
-        const api = fetch("https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-asv/books/john/chapters/3/verses/16.json", [])
-        .then((response) => {return response.json()});
-        
-      }, [selectedApi]);
+    function DemoPassage() {
+      return selectedApi;
     }
 
     return (
-      <div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        background: 'linear-gradient(to right, #45a247, #283c86)',
+        color: 'white' }} >
         <p>Change translation?.</p>
-        <select>
-          {bibleApis.map(api => <option key={api}>{api}</option>)}
+        <select onChange = {handleSelection}>
+          {selectorOptions.map(api => <option key={api} value={api}>{api}</option>)}
         </select>
+        <p><DemoPassage /></p>
       </div>
     )
   }
-
-  function DemoPassage() {
-    return verse1
-  }
-
+  
   return (
     <div>
       <h1>Settings</h1>
       <TranslationController />
     </div>
-  );
+  )
 }
 
 export default Settings;
