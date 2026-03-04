a5f26b4f8ff3e6a0b3d98cd91c1dea2462f3de8e
eunicehwang06 <eunicehwang06@gmail.com>
2026-03-04 01:30:33 +0000

    simple API fetch

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 function Settings() {
   const bibleApis = ['ESV API', 'KJV API', 'Mormon Bible API'];
+
+  function FetchTranslation() {
+    fetch('https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-kjv/books/john/chapters/3/verses/16.json)', []);
+  }
+
+  function Translation() {
+    useEffect(FetchTranslation, []);
+  }
+
+  function ChangeTranslation({translation}) {
+    return 
+  }
+
   return (
     <div>
       <h1>Settings</h1>
