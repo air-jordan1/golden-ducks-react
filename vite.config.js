2b0230e373821a7394ef87176c966d917dc8ced1
RainbowMellon <109363895+RainbowMellon@users.noreply.github.com>
2026-03-06 00:00:56 -0600

    Deployed the site

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: 'public', // This outputs "npm run build" to the "public" directory for deployment to firebase
+  },
 })
