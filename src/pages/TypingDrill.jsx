ae8842622d742e64193aaeebbc2a02b96e9e2951
Eunice Hwang <euniceshwang@gmail.com>
2026-04-17 12:58:35 -0500

    fix a little error in fetchPassage

--- src/pages/TypingDrill.jsx
+++ src/pages/TypingDrill.jsx
@@ -104,7 +104,7 @@ async function fetchPassage(parsed, translation) {
 
   const bibleId = await getTranslationId(translation);
   const base = `https://rest.api.bible/v1/bibles/${bibleId}`;
-  const modifiers = `?content-type=html&include-notes=false&include-titles=false&include-chapter-numbers=false&include-verse-numbers=false&include-verse-spans=false`
+  const modifiers = `?content-type=text&include-notes=false&include-titles=false&include-chapter-numbers=false&include-verse-numbers=false&include-verse-spans=false`
   const id = parsedRefToID(parsed);
 
   if (parsed.type === 'verse') {
